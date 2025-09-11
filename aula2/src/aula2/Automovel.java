01669bc0007e1033e9c9c13f9f72d523ecaed6a9
ZesseZ <caioaugusto.stos@gmail.com>
2025-09-11 09:04:53 -0300

    Atualizando o encapsulamento

--- aula2/src/aula2/Automovel.java
+++ aula2/src/aula2/Automovel.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Automovel {
-	public int autonomia;
-	public double preco;
-	public String marca;
-	public String modelo;
-	public int ano;
-	public int num;
+	private int autonomia;
+	private double preco;
+	private String marca;
+	private String modelo;
+	private int ano;
+	private int num;
 	
 	Scanner leitura = new Scanner(System.in);
 	Random aleat = new Random();
+	
+	private String lerStringEmMaiusculas(String prompt) {
+        System.out.println(prompt);
+        return leitura.next().toUpperCase();
+    }
+	
 	public void fichaTecnica(){
 		System.out.println("\n\nMarca: " + marca);
 		System.out.println("Modelo: " + modelo);
@@ -22,10 +28,8 @@ public class Automovel {
 	}
 	
 	public void defineAutomovel() {
-		System.out.println("\nInforme a marca do veículo: ");
-		marca = leitura.next();
-		System.out.println("\nInforme o modelo do veículo: ");
-		modelo = leitura.next();
+		marca = lerStringEmMaiusculas("\nInforme a marca do veículo: ");
+		modelo = lerStringEmMaiusculas("\nInforme o modelo do veículo: ");
 		System.out.println("\nInforme o ano do veículo: ");
 		ano = leitura.nextInt();
 		System.out.println("\nInforme o preço do veículo: ");
@@ -58,4 +62,54 @@ public class Automovel {
 	public void Som() {
 		System.out.println("som");
 	}
+
+	public int getAutonomia() {
+		return autonomia;
+	}
+
+	public void setAutonomia(int autonomia) {
+		this.autonomia = autonomia;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
 }
